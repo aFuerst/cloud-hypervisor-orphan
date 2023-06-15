d64316caf3818bdf1002f7f8a870cb278405a9d7
Yi Wang <foxywang@tencent.com>
2023-06-15 12:02:45 +0800

    devices: pvpanic: add method for DeviceManager
    Add method for DeviceManager to invoke.
    
    Signed-off-by: Yi Wang <foxywang@tencent.com>
    Signed-off-by: Rob Bradford <rbradford@rivosinc.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -2239,6 +2239,7 @@ mod unit_tests {
             user_devices: None,
             vdpa: None,
             vsock: None,
+            pvpanic: false,
             iommu: false,
             #[cfg(target_arch = "x86_64")]
             sgx_epc: None,
